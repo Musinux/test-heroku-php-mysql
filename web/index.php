4e20f47987c09b7cae6b037d99ad1cea40c08916
Louis Cherel <cherel.louis@gmail.com>
2020-10-01 16:06:56 +0200

    ça marche ?

--- web/index.php
+++ web/index.php
@@ -2,7 +2,9 @@
 <?php
 $dbopts = parse_url(getenv('DATABASE_URL'));
 
-$pdo = new PDO("mysql:host=".$dbopts['host'].";dbname=".$dbopts['path'].";charset=utf8mb4", $dbopts['user'], $dbopts['pass']);
+$out = "mysql:host=".$dbopts['host'].";dbname=".$dbopts['path'].";charset=utf8mb4", $dbopts['user'], $dbopts['pass'];
+
+$pdo = new PDO($out);
 
 $stmt = $pdo->query('INSERT INTO users (firstname, lastname) VALUES ("louis", "cherel")');
 
